4b5c0b2e6e16ad81beccc12d053137e12d5faf1b
Kaj Munhoz Arfvidsson <kajarfvidsson@gmail.com>
2021-03-15 21:41:59 +0100

    monitor and color fix

--- config/herbstluftwm/onedark.sh
+++ config/herbstluftwm/onedark.sh
@@ -29,15 +29,15 @@
 # " | Comment Grey | rgb(92, 99, 112)   | #5c6370 |
 # " +---------------------------------------------+
 
-black='#282c34'
-white='#abb2bf'
-red='#e06c75'
-darkred='#be5046'
-green='#98c379'
-yellow='#e5c07b'
-darkyellow='#d19a66'
-blue='#61afef'
-magenta='#c678dd'
-cyan='#56b6c2'
-darkgrey='#4b5263'
-grey='#5c6370'
+black="#282c34"
+white="#abb2bf"
+red="#e06c75"
+darkred="#be5046"
+green="#98c379"
+yellow="#e5c07b"
+darkyellow="#d19a66"
+blue="#61afef"
+magenta="#c678dd"
+cyan="#56b6c2"
+darkgrey="#4b5263"
+grey="#5c6370"
