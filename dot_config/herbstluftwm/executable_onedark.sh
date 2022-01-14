c80ebdccc151a5c84c888d71f4eb1e0eb441c698
Kaj Munhoz Arfvidsson <kajarfvidsson@gmail.com>
2022-01-14 06:44:16 +0100

    feat: added hlwm configs

---
+++ dot_config/herbstluftwm/executable_onedark.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/env bash
+
+# " OneDark
+black="#282c34"
+white="#abb2bf"
+
+red="#e06c75"
+darkred="#be5046"
+
+green="#98c379"
+
+yellow="#e5c07b"
+darkyellow="#d19a66"
+
+blue="#61afef"
+
+magenta="#c678dd"
+cyan="#56b6c2"
+
+darkgrey="#4b5263"
+grey="#5c6370"
